0a05d927bc677fb0e8ed6e65ab2ed81ac7e83199
AlleyTerry <art8689@nyu.edu>
2024-09-17 19:23:15 -0400

    battle works mostly

--- chasergame_GodotTest/Script/battle_scene.gd
+++ chasergame_GodotTest/Script/battle_scene.gd
@@ -2,17 +2,26 @@ extends Node2D
 
 @export var enemy: Resource = null
 @export var playerHealth = 35
+@onready var letterArray = ["y","u","i","o","p","h","j","k","l","b","n","m"]
+var currentLetter = 0
+var letterToPress = ""
+@export var testPass = false
+@export var startMinigame = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	enemy.health = 55
+	enemy.health = 20
 	print (enemy.health)
 	$Enemy/CollisionShape2D/Sprite2D.texture = enemy.texture
+	letterToPress = letterArray[0]
+	$letterShower.text = "press and hold: q w e r"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-
+	if (startMinigame == true):
+		doMinigame()
+	$letterShower/timerProgress.value = $letterShower/Timer.get_process_delta_time()
+	
 func enemyTurn():
 	playerHealth -= 10
 	$AnimationPlayer.play("enemy_attack")
@@ -21,21 +30,74 @@ func enemyTurn():
 	await $AnimationPlayer.animation_finished
 
 func _on_attack_button_pressed() -> void:
-	enemy.health -= 10
-	$AnimationPlayer.play("player_attack")
-	await $AnimationPlayer.animation_finished
-	$AnimationPlayer.play("EnemyHurt")
-	await $AnimationPlayer.animation_finished
-	print(enemy.health)
-	if (enemy.health <= 0):
-		$AnimationPlayer.play("EnemyDied")
+	$letterShower/TextureProgressBar.value = 0
+	startMinigame = true
+	
+	
+
+func PlayerAttack():
+	if (testPass == true):
+		$letterShower.visible = false
+		testPass == false
+		enemy.health -= 10
+		$AnimationPlayer.play("player_attack")
 		await $AnimationPlayer.animation_finished
-		await get_tree().create_timer(.25).timeout
-		get_tree().change_scene_to_file("res://Scenes/game_scene.tscn")
-	else: 
-		enemyTurn()
+		$AnimationPlayer.play("EnemyHurt")
+		await $AnimationPlayer.animation_finished
+		print(enemy.health)
+		if (enemy.health <= 0):
+			$AnimationPlayer.play("EnemyDied")
+			await $AnimationPlayer.animation_finished
+			await get_tree().create_timer(.25).timeout
+			get_tree().change_scene_to_file("res://Scenes/game_scene.tscn")
+		else: 
+			enemyTurn()
+
+	
+func doMinigame():
+	$letterShower.visible =  true
+	while (Input.is_action_pressed("w") && Input.is_action_pressed("e") && Input.is_action_pressed("q") && Input.is_action_pressed("r")):
+		$letterShower.text = letterToPress
+		StepThrough()
+		if ($letterShower/TextureProgressBar.value >= 100):
+			testPass = true
+			TurnReset()
+			PlayerAttack()
+		break
+	
+
+func StepThrough():
+	if (Input.is_action_just_pressed(letterToPress)):
+		var tween = create_tween()
+		$letterShower/Timer.start()
+		if currentLetter <= 3:
+			var randomLetter = letterArray.pick_random()
+			#letterToPress = letterArray[currentLetter]
+			$letterShower.text = randomLetter
+			letterToPress = randomLetter
+			print(letterToPress)
+			currentLetter += 1
+			tween.tween_property($letterShower/TextureProgressBar,"value", $letterShower/TextureProgressBar.value + 20, 0.5)
+			
+		else:
+			tween.tween_property($letterShower/TextureProgressBar,"value", $letterShower/TextureProgressBar.value + 20, 0.5)
+			
+		
 
 
 func _on_talk_button_pressed() -> void:
 	pass
 	
+
+
+func _on_timer_timeout() -> void:
+	TurnReset()
+	enemyTurn()
+	
+func TurnReset():
+	startMinigame = false
+	$letterShower/TextureProgressBar.value = 0
+	currentLetter = 0
+	$letterShower/Timer.stop()
+	$letterShower.text = "press and hold: q w e r"
+	$letterShower.visible = false
--- chasergame_GodotTest/Script/pop_up_text.gd
+++ chasergame_GodotTest/Script/pop_up_text.gd
@@ -3,29 +3,37 @@ extends Label
 @onready var letterArray = ["y","u","i","o","p","h","j","k","l","b","n","m"]
 var currentLetter = 0
 var letterToPress = ""
+@export var testPass = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	letterToPress = letterArray[0]
 	$".".text = "press and hold: q w e r"
+	
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	#StepThrough()
 	#StepThroughConinuous()
-	StepThroughHoldPress()
+	#if (Input.is_action_pressed("click")):
+	#StepThroughHoldPress()
+	$timerProgress.value = $Timer.time_left
+	
 
 func StepThrough():
-	if (Input.is_action_pressed(letterToPress)):
+	if (Input.is_action_just_pressed(letterToPress)):
 		var tween = create_tween()
-		if currentLetter < letterArray.size()-1:
+		$Timer.start()
+		if currentLetter < 5:
 			letterToPress = letterArray[currentLetter]
 			$".".text = letterArray[currentLetter + 1]
 			letterToPress = letterArray[currentLetter + 1]
 			print(letterToPress)
 			currentLetter += 1
 			tween.tween_property($TextureProgressBar,"value", $TextureProgressBar.value + 25, 0.5)
+			$Timer.start()
 		else:
 			tween.tween_property($TextureProgressBar,"value", $TextureProgressBar.value + 25, 0.5)
 			$".".text = "congrats you know your abcs"
@@ -41,10 +49,6 @@ func StepThroughConinuous():
 					tween.tween_property($TextureProgressBar,"value", $TextureProgressBar.value + 100, 0.5)
 	if ($TextureProgressBar.value >= 100):
 					$".".text = "congrats you know your abcs"
-func StepThroughHoldPress():
-	while (Input.is_action_pressed("w") && Input.is_action_pressed("e") && Input.is_action_pressed("q") && Input.is_action_pressed("r")):
-		$".".text = letterToPress
-		StepThrough()
-		if ($TextureProgressBar.value >= 100):
-			$".".text = "ur a wizard wrow..."
-		break
+
+	
+	
