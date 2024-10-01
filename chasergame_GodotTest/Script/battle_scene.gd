f29348494c43ee8349f56d38d97c509a9bf8e71b
AlleyTerry <art8689@nyu.edu>
2024-10-01 17:33:17 -0400

    battle done

--- chasergame_GodotTest/Script/battle_scene.gd
+++ chasergame_GodotTest/Script/battle_scene.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 @export var enemy: Resource = null
 @export var playerHealth = 3
-@onready var letterArray = ["y","u","i","o","p","h","j","k","l","b","n","m"]
+@onready var letterArray = ["Left","Right","Up","Down"]
 var currentLetter = 0
 var letterToPress = ""
 @export var testPass = false
@@ -16,71 +16,79 @@ var timerCheck = true
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Dialogic.signal_event.connect(OnDialogicSignal)
+	Dialogic.signal_event.connect(AttackSignal)
+	Dialogic.signal_event.connect(EnemySignal)
+	Dialogic.start("timeline")
 	$Node2D.visible = true
 	enemy.health = 3
-	$Enemy/CollisionShape2D/Sprite2D.texture = enemy.texture
+	$Enemy/Sprite2D.texture = enemy.texture
 	letterToPress = letterArray[0]
-	$letterShower.text = "press and hold: q w e r"
+	$letterShower.text = "press and hold: w a d"
 	newRandomLetter = letterArray.pick_random()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if defenseCheck == true:
+		DefenseQTE()
 	if (startMinigame == true):
 		doMinigame()
-	if (defenseCheck == true):
-		DefenseQTE()
+	
 	$letterShower/timerProgress.value = $letterShower/Timer.get_process_delta_time()
 	$EnemyQTE/EnemyTimerProgress.value = $EnemyQTE/EnemyTimer.time_left
+
+func _physics_process(delta: float) -> void:
+	pass
 	
 	
 func enemyTurn():
 	enemyTurnCheck = false
 	$EnemyQTE/EnemyTimer.stop()
 	$EnemyQTE.visible = false
-	playerHealth -= 1
-	print("player health ", playerHealth)
-	$AnimationPlayer.play("enemy_attack")
-	await $AnimationPlayer.animation_finished
-	$AnimationPlayer.play("PlayerHurt")
-	await $AnimationPlayer.animation_finished
-	$Node2D.visible = true
-	if playerHealth <= 0:
-		$EnemyQTE.visible = false
-		$EnemyQTE.text = "U DEAD"
-	else:
-		defenseCheck = false
-		enemyTurnCheck = false
-		TurnReset()
+	#playerHealth -= 1
+	#print("player health ", playerHealth)
+	$EnemyAnimationPlayer.play("EnemyAttack")
+	#$AnimationPlayer.play("enemy_attack")
+	await $EnemyAnimationPlayer.animation_finished
+	
 	
 
 func OnDialogicSignal(argument: String):
 	#remember to pause dialogue
 	if argument == "startBattle1":
-		$letterShower.visible = false
-		Dialogic.start("timeline")
+		print("signal was fired")
+		Dialogic.start("TextChoices1")
 	
 
 func DefenseQTE():
 	$EnemyQTE.visible = true
-	if (Input.is_action_pressed("s")):
-		$EnemyQTE.text = newRandomLetter
-		if timerCheck == true:
-			$EnemyQTE/EnemyTimer.start()
-			timerCheck = false
-		$EnemyQTE.text = newRandomLetter
-		if (Input.is_action_just_pressed(newRandomLetter)):
-			$EnemyQTE/EnemyTimer.stop()
-			enemyTurnCheck = false
-			defenseCheck = false
-			$EnemyQTE.text = "MISS"
-			await get_tree().create_timer(1).timeout
-			$EnemyQTE.visible = false
-			$Node2D.visible = true
-		elif enemyTurnCheck == true:
-			defenseCheck = false
-			$EnemyQTE/EnemyTimer.stop()
-			$EnemyQTE.visible = false
-			enemyTurn()
+	# if (Input.is_action_pressed("s")):
+	$EnemyQTE.text = newRandomLetter
+	if timerCheck == true:
+		$EnemyQTE/EnemyTimer.start()
+		timerCheck = false
+	#$EnemyQTE.text = newRandomLetter
+	if (Input.is_action_pressed(newRandomLetter+"Action")):
+		$EnemyQTE/EnemyTimer.stop()
+		enemyTurnCheck = false
+		defenseCheck = false
+		$EnemyQTE.text = "MISS"
+		await get_tree().create_timer(1).timeout
+		$EnemyQTE.visible = false
+		$Node2D.visible = true
+		Dialogic.start("TextChoices1")
+
+func AttackSignal(argument: String):
+	if argument == "AttackChoice":
+		$Node2D.visible = false
+		$letterShower/TextureProgressBar.value = 0
+		startMinigame = true
+
+func EnemySignal(argument: String):
+	if argument == "EnemyTurn":
+		$Node2D.visible = false
+		$letterShower/TextureProgressBar.value = 0
+		defenseCheck = true
+	
 
 func _on_attack_button_pressed() -> void:
 	$Node2D.visible = false
@@ -91,6 +99,7 @@ func _on_attack_button_pressed() -> void:
 
 func PlayerAttack():
 	if (testPass == true):
+		$Player.visible = true
 		$letterShower.visible = false
 		testPass = false
 		enemy.health -= 1
@@ -100,6 +109,7 @@ func PlayerAttack():
 		$AnimationPlayer.play("EnemyHurt")
 		await $AnimationPlayer.animation_finished
 		print(enemy.health)
+		$Player.visible = false
 		if (enemy.health <= 0):
 			$AnimationPlayer.play("EnemyDied")
 			await $AnimationPlayer.animation_finished
@@ -109,12 +119,12 @@ func PlayerAttack():
 			defenseCheck = true
 			$EnemyQTE.text = "hold s"
 			timerCheck = true
-			DefenseQTE()
+			enemyTurn()
 
 	
 func doMinigame():
 	$letterShower.visible =  true
-	while (Input.is_action_pressed("w") && Input.is_action_pressed("e") && Input.is_action_pressed("q") && Input.is_action_pressed("r")):
+	while (Input.is_action_pressed("w") && Input.is_action_pressed("a") && Input.is_action_pressed("d")):
 		$letterShower.text = letterToPress
 		StepThrough()
 		if ($letterShower/TextureProgressBar.value >= 100):
@@ -125,7 +135,7 @@ func doMinigame():
 	
 
 func StepThrough():
-	if (Input.is_action_just_pressed(letterToPress)):
+	if (Input.is_action_just_pressed(letterToPress + "Action")):
 		var tween = create_tween()
 		$letterShower/Timer.start()
 		if currentLetter <= 3:
@@ -141,9 +151,6 @@ func StepThrough():
 			tween.tween_property($letterShower/TextureProgressBar,"value", $letterShower/TextureProgressBar.value + 20, 0.5)
 
 
-func _on_talk_button_pressed() -> void:
-	pass
-	
 
 
 func _on_timer_timeout() -> void:
@@ -151,8 +158,8 @@ func _on_timer_timeout() -> void:
 	await get_tree().create_timer(.5).timeout
 	defenseCheck = true
 	timerCheck = true
-	$EnemyQTE.text = "hold s"
-	DefenseQTE()
+	enemyTurn()
+	
 	
 func TurnReset():
 	startMinigame = false
@@ -160,11 +167,28 @@ func TurnReset():
 	$letterShower/TextureProgressBar.value = 0
 	currentLetter = 0
 	$letterShower/Timer.stop()
-	$letterShower.text = "press and hold: q w e r"
+	$letterShower.text = "press and hold: w a d "
 	$letterShower.visible = false
 	
 
 
+
+
 func _on_enemy_timer_timeout() -> void:
-	enemyTurn()
-	
+	defenseCheck = false
+	$EnemyQTE/EnemyTimer.stop()
+	$AnimationPlayer.play("PlayerHurt")
+	await $AnimationPlayer.animation_finished
+	playerHealth -= 1
+	print("player health ", playerHealth)
+	TurnReset()
+	$Node2D.visible = true
+	Dialogic.start("TextChoices1")
+
+
+
+
+func _on_player_area_area_entered(area: Area2D) -> void:
+	if area.name == "EnemyArea":
+		defenseCheck = true
+		print(area.name)
