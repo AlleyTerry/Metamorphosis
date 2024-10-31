8f2f76563337235aa71b2c5fb6147c1ecaa9bf36
AlleyTerry <art8689@nyu.edu>
2024-10-31 17:24:48 -0400

    dresser is moveable
    and the room door leads straight to the battle scene

--- chasergame_GodotTest/Script/imeris_room.gd
+++ chasergame_GodotTest/Script/imeris_room.gd
@@ -3,8 +3,16 @@ extends Node2D
 func _ready() -> void:
 	if DoorManager.spawnTag != null:
 		onleveSpawn(DoorManager.spawnTag)
+		
+	Dialogic.signal_event.connect(moveDresser)
 
 func onleveSpawn(destinationTag: String):
 	var door_path = "Doors/Door_" + destinationTag
 	var door = get_node(door_path) as Door
 	DoorManager.triggerPlayerSpawn(door.spawn.global_position, door.spawnDirection)
+	
+
+func moveDresser (argument: String):
+	if argument == "moveDresser":
+		var tween = create_tween()
+		tween.tween_property($Dresser, 'position:y', 268, 0.5)
