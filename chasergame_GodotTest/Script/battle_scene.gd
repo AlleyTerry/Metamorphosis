a479ee0f726523587ee2e8b2166916a2cc38096c
ESP-YRI <sap637@nyu.edu>
2024-11-04 16:04:45 -0500

    Tutorializing dialogue added
    Now the player will know what to do

--- chasergame_GodotTest/Script/battle_scene.gd
+++ chasergame_GodotTest/Script/battle_scene.gd
@@ -327,6 +327,8 @@ func _on_timer_timeout() -> void:
 	var tween = create_tween()
 	tween.tween_property($Imeris, 'position:y',  tweenPosition, 0.5)
 	inputCheck = false
+	upCheck = false
+	firstCheck = true
 	TurnReset()
 	Dialogic.VAR.linAttack = true
 	$AnimationPlayer.play("PlayerHurt")
